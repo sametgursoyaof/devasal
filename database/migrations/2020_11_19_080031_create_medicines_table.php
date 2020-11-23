3441fbe400db9511a63037731bf0bd87e5296ca8
sametgursoyaof <70822898+sametgursoyaof@users.noreply.github.com>
2020-11-23 11:35:32 +0300

    Update 2020_11_19_080031_create_medicines_table.php

--- database/migrations/2020_11_19_080031_create_medicines_table.php
+++ database/migrations/2020_11_19_080031_create_medicines_table.php
@@ -39,6 +39,6 @@ class CreateMedicinesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('devasal');
+        Schema::dropIfExists('medicines');
     }
 }
