36e4f6cf967c9989070df64659e266799fc30d2d
gulinkaradeniz <70319263+gulinkaradeniz@users.noreply.github.com>
2020-12-08 18:50:04 +0300

    medicines

---
+++ app/Medicines.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+use App\Companies;
+use App\Images;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+class Medicines extends Model
+{
+    protected $fillable = [
+        'name',
+        'active_ingredient',
+        'description',
+        'formula',
+        'pharmacological',
+        'indication',
+        'kontrendikasyon',
+        'warning',
+        'side_effects',
+        'usage',
+        'extra_information',
+        'barcode',
+        'companies_id',
+        'slug',
+        'status'
+    ];
+    public function owner()
+    {
+        return $this->belongsTo(Companies::class, 'companies_id','id');
+    }
+    public function images()
+    {
+        return $this->hasMany(Images::class,'medicine_id','id');
+    }
+}
